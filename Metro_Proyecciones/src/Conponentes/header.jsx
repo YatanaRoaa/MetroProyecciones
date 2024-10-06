6266464ae4f56757dc946aafd2bdcbea3487bbc2
Luis Angel <luisgelipe.12340@gmail.com>
2024-10-05 22:42:49 -0600

    creacion de paguina funciones

--- Metro_Proyecciones/src/Conponentes/header.jsx
+++ Metro_Proyecciones/src/Conponentes/header.jsx
@@ -1,12 +1,13 @@
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import Icon from './icon';
 import './../assets/css/header.css';
-const Header = () => {
+// eslint-disable-next-line react/prop-types
+const Header = ({nombreTitulo}) => {
     return (
         <>
         <section className='header'> 
             <button className='header-button-menu'><Icon icon={faBars} /></button>
-            <h1 className='h1'>Registrar funcion</h1>
+            <h1 className='h1'>{nombreTitulo}</h1>
         </section>
         </>
     )
--- Metro_Proyecciones/src/Paginas/RegistrarFuncion.jsx
+++ Metro_Proyecciones/src/Paginas/RegistrarFuncion.jsx
@@ -1,9 +1,55 @@
 import Header from '../Conponentes/header'
+import './../assets/css/RegistrarFuncion.css'
 
 const RegistrarFuncion = () => {
     return (
         <>
-            <Header />
+            <Header nombreTitulo={'Registrar Función'} />
+            <section className='registro-funcion'>
+                <form className='registro-funcion-form'>
+                    <fieldset className='registro-funcion-form-nombre'>
+                        <label htmlFor="nombreFuncion">Nombre</label>
+                        <input 
+                        className='input-text input-text-nombre'
+                        id="nombreFuncion" 
+                        type="text" 
+                        placeholder="The GoodFather"
+                    />
+                    </fieldset>
+
+                    <fieldset className='registro-funcion-form-fecha'>
+                        <label htmlFor="fecha">Fecha</label>
+                        <input name="fecha" type="text" className='input-text-fecha'/>
+                        <input name="fecha" type="text" className='input-text-fecha'/>
+                        <input name="fecha" type="text" className='input-text-fecha'/>                        
+                    </fieldset> 
+
+                    <fieldset className='registro-funcion-form-horario'>
+                        <label >Horario</label>
+                        <button type="button" >10:00 </button>
+                        <button type="button" >13:00 </button>
+                        <button type="button" >16:00 </button>
+                        <button type="button" >19:00 </button>
+                    </fieldset>
+                    
+                    <fieldset className='registro-funcion-form-boleto'>
+                        <label htmlFor="costoBoleto">Costo De Boleto</label>
+                        <input id="costoBoleto" type="text" className='input-text'/>
+                    </fieldset>
+
+                    <fieldset className='registro-funcion-form-duracion'>
+                        <label htmlFor="duracion">Duración</label>
+                        <input id="duracion" type="text" className='input-text'/>
+                        <span>Min</span>
+                    </fieldset>
+
+                    <fieldset className='registro-funcion-form-submit'>
+                        <button>Cancelar</button>
+                        <input type="submit" value="Enviar" />
+
+                    </fieldset>
+                </form>
+            </section>
         </>
     )
 }
